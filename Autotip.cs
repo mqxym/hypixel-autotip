56111373ef2d410fd8b804cfbdad8a4bec47f943
Maxim Schulz <maxim@omg.lol>
2023-08-17 15:48:21 +0200

    rewritten tip loop

--- Autotip.cs
+++ Autotip.cs
@@ -6,79 +6,76 @@
 
 class Autotip : ChatBot
 {
-    	
-        /*
-            IDEAS:
-            - Use another player when target player has gone offline
 
-        */
+    /*
+        IDEAS:
+        - Use another player when target player has gone offline
 
-        private readonly string username = "your_username";
-        private int count = 0;
-        private readonly int startrun = 50;
-        private readonly int tipDelay = 10;
-        private int tipCount = 0;
-        private bool tippingComplete = false;
-        private readonly string[] games = new string[] {"bsg", "mw", "tnt", "cops", "uhc", "war", "sw", "smash" , "classic", "arcade"};
-        private string[] onlinePlayers { get; set; }
+    */
 
-        public override void Update()
+    private readonly string username = "wobblterror";
+    private int count = 0;
+    private readonly int startrun = 50;
+    private readonly int tipDelay = 10;
+    private int tipCount = 0;
+    private int tipCountTarget = 9;
+    private bool tippingComplete = false;
+    private readonly string[] games = new string[] { "bsg", "mw", "tnt", "cops", "uhc", "war", "sw", "smash", "classic", "arcade" };
+    private string[] onlinePlayers { get; set; }
+
+    public override void Update()
+    {
+        count++;
+
+        if (count >= startrun)
         {
-            count++;
 
-            if (count >= startrun)
+            if (tippingComplete)
             {
-
-                if (tippingComplete)
+                //After ~3700 seconds reset the counter and tip again
+                if (count >= 3700 * 10)
                 {
-                    //After ~3700 seconds reset the counter and tip again
-                    if (count >= 3700*10)
-                    {
-                        tippingComplete = false;
-                        count = 0;
-                    } else
-                    {
-                        return;
-                    }
+                    tippingComplete = false;
+                    count = 0;
                 }
-                //After 5 seconds join bedwars lobby
-                if (count == startrun)
-                {       
-                    SendText("/lobby bedwars");
-                }
-                //After 15 seconds tip the first player, wait tipDelay and tip the next player
-                if (count == startrun + tipDelay*10)
+                else
                 {
-                    //get filtered list of online Players
+                    return;
+                }
+            }
+            //After 5 seconds join bedwars lobby
+            if (count == startrun)
+            {
+                SendText("/lobby bedwars");
+            }
+            //After 15 seconds tip the first player, wait tipDelay and tip the next player
+            if (count == (startrun + tipDelay * 10 + tipCount * 6 * 10))
+            {
+                //get filtered list of online Players at the first tip wave
+                if (count == startrun + tipDelay * 10)
+                {
                     onlinePlayers = GetOnlinePlayers().Where(item => item != username).ToArray();
+                    //do 10 tips or the ammount of players in the lobby
+                    tipCountTarget = Math.Min(9, onlinePlayers.Length - 1);
+                }
 
-                    if (tipCount < onlinePlayers.Length)
-                    {
-                        TipPlayer(onlinePlayers[tipCount], games[tipCount]);
-                    }
-                    
-                    tipCount++;
-                } else if(count == (startrun + tipDelay*10 + tipCount*6*10)) 
+                if (tipCount <= tipCountTarget)
                 {
-                    if (tipCount < onlinePlayers.Length)
-                    {
-                        TipPlayer(onlinePlayers[tipCount], games[tipCount]);
-                    }
+                    TipPlayer(onlinePlayers[tipCount], games[tipCount]);
                     tipCount++;
-
-                    //After 10 tips finish the round
-                    if(tipCount > 9)
-                    {
-                        tipCount = 0;
-                        tippingComplete = true;
-                    }
+                } 
+                else 
+                {
+                    tipCount = 0;
+                    tippingComplete = true;
                 }
             }
         }
-
-        private void TipPlayer(string playerName, string gameName)
-        {
-            //LogToConsole("Tip " + playerName + " " + gameName);
-            SendText("/tip " + playerName + " " + gameName);
-        }
     }
+
+    private void TipPlayer(string playerName, string gameName)
+    {
+        LogToConsole("Tip " + playerName + " " + gameName);
+        //SendText("/tip " + playerName + " " + gameName);
+    }
+}
